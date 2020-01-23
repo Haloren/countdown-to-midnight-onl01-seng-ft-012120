fd855414366295d8f387ca7de66d4b6960a832cd
Kyle McKenna <mckennakyler@gmail.com>
2020-01-23 15:49:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,11 @@ def countdown(number = 10)
   return "HAPPY NEW YEAR!"
 end
 countdown
-def countdown_with_sleep(5)
-  
+def countdown_with_sleep(number = 10)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  sleep(5)
+  return "HAPPY NEW YEAR!"
 end
